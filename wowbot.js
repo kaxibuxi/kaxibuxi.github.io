3062316e9e40612abedb24cd22d2b18937f7c21f
kaxibuxi <76875142+kaxibuxi@users.noreply.github.com>
2021-04-02 17:46:32 +0300

    Create wowbot.js

---
+++ wowbot.js
@@ -0,0 +1,96 @@
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "W";
+}, 200);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Wo";
+}, 400);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "WoW";
+}, 600);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "WoW ";
+}, 800);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "WoW B";
+}, 1000);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "WoW Bo";
+}, 1200);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "WoW Bot";
+}, 1400);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "WoW Bot:";
+}, 1600);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "WoW Bot: ";
+}, 1800);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "WoW Bot: E";
+}, 2000);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "WoW Bot: Eğ";
+}, 2200);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "WoW Bot: Eğl";
+}, 2400);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "WoW Bot: Eğle";
+}, 2600);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "WoW Bot: Eğlen";
+}, 2800);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "WoW Bot: Eğlenc";
+}, 3000);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "WoW Bot: Eğlence";
+}, 3200);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "WoW Bot: Eğlencel";
+}, 3400);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "WoW Bot: Eğlencel";
+}, 3600);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "WoW Bot: Eğlenceli";
+}, 3800);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "WoW Bot: Eğlenceli ";
+}, 4000);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "WoW Bot: Eğlenceli D";
+}, 4200);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "WoW Bot: Eğlenceli Di";
+}, 4400);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "WoW Bot: Eğlenceli Dis";
+}, 4600);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "WoW Bot: Eğlenceli Disc";
+}, 4800);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "WoW Bot: Eğlenceli Disco";
+}, 5000);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "WoW Bot: Eğlenceli Discor";
+}, 5200);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "WoW Bot: Eğlenceli Discord";
+}, 5400);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "WoW Bot: Eğlenceli Discord ";
+}, 5600);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "WoW Bot: Eğlenceli Discord B";
+}, 5800);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "WoW Bot: Eğlenceli Discord Bo";
+}, 6000);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "WoW Bot: Eğlenceli Discord Bot";
+}, 6200);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "WoW Bot: Eğlenceli Discord Botu";
+}, 6400);
