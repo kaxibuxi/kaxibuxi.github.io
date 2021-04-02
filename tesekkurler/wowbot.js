646424d2add139cca698ffe933d1fbefc12d3601
kaxibuxi <76875142+kaxibuxi@users.noreply.github.com>
2021-04-02 23:40:50 +0300

    Create wowbot.js

---
+++ tesekkurler/wowbot.js
@@ -0,0 +1,96 @@
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "D";
+}, 200);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Da";
+}, 400);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Dav";
+}, 600);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Dave";
+}, 800);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Davet";
+}, 1000);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Davet ";
+}, 1200);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Davet E";
+}, 1400);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Davet Et";
+}, 1600);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Davet Ett";
+}, 1800);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Davet Etti";
+}, 2000);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Davet Ettiğ";
+}, 2200);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Davet Ettiği";
+}, 2400);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Davet Ettiğin";
+}, 2600);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Davet Ettiğin ";
+}, 2800);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Davet Ettiğin İ";
+}, 3000);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Davet Ettiğin İç";
+}, 3200);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Davet Ettiğin İçi";
+}, 3400);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Davet Ettiğin İçin";
+}, 3600);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Davet Ettiğin İçin ";
+}, 3800);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Davet Ettiğin İçin T";
+}, 4000);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Davet Ettiğin İçin Te";
+}, 4200);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Davet Ettiğin İçin Teş";
+}, 4400);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Davet Ettiğin İçin Teşek";
+}, 4600);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Davet Ettiğin İçin Teşekk";
+}, 4800);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Davet Ettiğin İçin Teşekkü";
+}, 5000);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Davet Ettiğin İçin Teşekkür";
+}, 5200);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Davet Ettiğin İçin Teşekkürl";
+}, 5400);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Davet Ettiğin İçin Teşekkürle";
+}, 5600);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Davet Ettiğin İçin Teşekkürler";
+}, 5800);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Davet Ettiğin İçin Teşekkürler ";
+}, 6000);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Davet Ettiğin İçin Teşekkürler <";
+}, 6200);
+setTimeout(function(){ 
+    document.getElementById("wowbot").innerHTML = "Davet ettiğin için teşekkürler <3";
+}, 6400);
